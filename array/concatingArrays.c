5bbe7201c05c78404f985986cb5172dc286e5f2e
sapirani <sapirani@post.bgu.ac.il>
2025-11-29 15:14:10 +0200

    add task description

--- array/concatingArrays.c
+++ array/concatingArrays.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+
+Example 2:
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Constraints:
+    n == nums.length
+    1 <= n <= 1000
+    1 <= nums[i] <= 1000
+
+* Note: The returned array must be malloced, assume caller calls free().
+*/
+
 int *getConcatenation(int *nums, int numsSize, int *returnSize)
 {
     int *ans = (int *)malloc(numsSize * 2 * sizeof(int));
